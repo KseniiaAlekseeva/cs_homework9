1c24d66795f4cc39d7ef30f1efc8bbbd682a77fe
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 17:17:02 +0300

    Add task68

---
+++ task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Program calculating Akkerman function for two positive numbers M and N.
+
+long Akkerman(long n, long m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (m == 0)
+        return Akkerman(n - 1, 1);
+    else
+        return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+Console.Write("Enter the first natural number N: ");
+long n = long.Parse(Console.ReadLine());
+
+Console.Write("Enter the second natural number M: ");
+long m = long.Parse(Console.ReadLine());
+
+Console.WriteLine($"Akkerman({n},{m}) is {Akkerman(n, m)}.");
