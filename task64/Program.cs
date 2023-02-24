def83fd31740cc40cd28db6d1beebfd0ab610dd9
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 16:47:33 +0300

    Add task64

---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program outputs all natural numbers from N to 1
+
+void PrintNumbers(int n)
+{
+    Console.Write(n + ", ");
+    if (n == 1)
+        return;
+    else
+        PrintNumbers(n - 1);
+}
+
+
+Console.Write("Enter the natural number N: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(n);
+
